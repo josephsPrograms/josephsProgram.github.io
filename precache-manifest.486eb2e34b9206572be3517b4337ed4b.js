58e464774825d6f6b87ec1822c0be9cce9e58f5c
DESKTOP-01M36DM\josep <josephsPrograms>
2019-11-01 14:48:40 -0700

    Updates

---
+++ precache-manifest.486eb2e34b9206572be3517b4337ed4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0db735ff6612718743c8a592aaed315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd9ee29a62e77d05448",
+    "url": "/static/css/main.367e72dc.chunk.css"
+  },
+  {
+    "revision": "e2951a504a40cca48a9d",
+    "url": "/static/js/2.565801ce.chunk.js"
+  },
+  {
+    "revision": "cfd9ee29a62e77d05448",
+    "url": "/static/js/main.07857f14.chunk.js"
+  },
+  {
+    "revision": "eb9c9f0dd9c4abd7f442",
+    "url": "/static/js/runtime-main.46bfa318.js"
+  }
+]);
